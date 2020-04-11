549c5f0ab04c475b136821d7f0e5f12bab521b5b
jangabler <1701963+jangabler@users.noreply.github.com>
2020-04-11 02:07:17 +0200

    Add xlsxgen to generate test data spreadsheets

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 xlsx2sql: ./cmd/xlsx2sql/main.go
 	@go build -o $@ $<
 
+xlsxgen: ./cmd/xlsxgen/main.go
+	@go build -o $@ $<
+
 test:
 	@go test -covermode=count -coverprofile=count.out ./...
 
---
+++ cmd/xlsxgen/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/360EntSecGroup-Skylar/excelize/v2"
+)
+
+var (
+	Number = flag.Int(
+		"n",
+		10,
+		"Number of generated Excel files.",
+	)
+)
+
+func main() {
+	flag.Parse()
+	workingDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	for i := 1; i <= *Number; i++ {
+		xlsx := excelize.NewFile()
+		xlsx.NewSheet("Sheet1")
+		err = xlsx.SetCellStr("Sheet1", "A1", getRandString())
+		if err != nil {
+			panic(err)
+		}
+		err = xlsx.SetCellInt("Sheet1", "B1", getRandInt())
+		if err != nil {
+			panic(err)
+		}
+		err = xlsx.SetCellBool("Sheet1", "C1", getRandBool())
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf(
+			"%s/test/data/spreadsheet%d.xlsx",
+			workingDir,
+			i,
+		)
+		err := xlsx.SaveAs(fileName)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func getRandString() string {
+	const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	b := make([]byte, 10)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
+
+func getRandInt() int {
+	return rand.Intn(100)
+}
+
+func getRandBool() bool {
+	return rand.Intn(100) > 50
+}

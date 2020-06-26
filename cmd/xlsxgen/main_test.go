23bb616ce5ecc7648e0783aa390d92bf7f687480
jangabler <1701963+jangabler@users.noreply.github.com>
2020-06-26 20:27:08 +0200

    Update xlsxgen tests

--- cmd/xlsxgen/main_test.go
+++ cmd/xlsxgen/main_test.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
-	// TODO: implement unit test
+	m.Run()
+}
+
+func TestGetRandString(t *testing.T) {
+	expected := "abc"
+	actual := getRandString()
+	assert.IsType(t, expected, actual)
+}
+
+func TestGetRandInt(t *testing.T) {
+	min, max := 0, 100
+	expected := 1
+	actual := getRandInt()
+	assert.IsType(t, expected, actual)
+	assert.GreaterOrEqual(t, actual, min)
+	assert.LessOrEqual(t, actual, max)
+}
+
+func TestGetRandBool(t *testing.T) {
+	expected := true
+	actual := getRandBool()
+	assert.IsType(t, expected, actual)
 }
